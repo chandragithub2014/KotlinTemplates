4a635220c6830f61b5045b91dd7ed969908d6749
chandragithub2014 <b.chandrasaimohan@gmail.com>
2018-12-13 10:55:06 +0530

    First Commit React Native

---
+++ MyFirstReact/myApp/myViews/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+
+export class Login extends React.Component{
+    render(){
+        return(
+             <View>
+                 <Text>This is Login Home Page</Text>
+                 <Text>Two Text Inputs and a Button</Text>
+             </View>
+        );
+    }
+}
